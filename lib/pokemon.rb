666ef55ef82cc80cd71cb8482d1e2bb664dfcfa9
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-10 03:22:32 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,7 +4,8 @@ class Pokemon
 
   # attr_accessor :name, :type, :db
 
-  def initialize(name, type, db)
+  def initialize(id, name, type, db)
+    @id = id
     @name = name
     @type = type
     @db = db
