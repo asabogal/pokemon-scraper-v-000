479d4a8cb6ef8a8558b6364f7c02f89786006623
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-10 05:05:06 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -23,11 +23,9 @@ class Pokemon
     Pokemon.new(id: find_pokemon[0], name: find_pokemon[1], type: find_pokemon[2], db: db)
   end
 
-  def change_hp(new_hp, db)
-    pikachu = "UPDATE pokemon SET hp = 59  WHERE id = 1"
-    magikarp = "UPDATE pokemon SET hp = 0  WHERE id = 2"
-    db.execute(pikachu, magikarp, new_hp, self.id)
-
+  def alter_hp(hp, db)
+    sql = "UPDATE pokemon SET hp = ? WHERE id = ?"
+    db.execute(sql, hp, self.id)
   end
 
 end
