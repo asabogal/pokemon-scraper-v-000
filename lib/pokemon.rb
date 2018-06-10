f455e45b215fc7cfd688584d9fe33a0739c27833
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-10 03:49:23 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -4,7 +4,7 @@ class Pokemon
   @@all = []
 
   attr_accessor :id, :name, :type, :db
-  def initialize(name, type, db)
+  def initialize(name = "david", type = "coach", db = {})
     # binding.pry
     @name = name
     @type = type
