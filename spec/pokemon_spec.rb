d45de823aa10962a8dc031ebbcce6f4c239ad0ba
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-10 05:11:18 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -40,8 +40,8 @@ describe "Pokemon" do
     end
   end
 
-  describe "BONUS" do
-
+  # describe "BONUS" do
+  #
   #   before do
   #     @sql_runner.execute_create_hp_column
   #     Pokemon.save('Pikachu', 'electric', @db)
